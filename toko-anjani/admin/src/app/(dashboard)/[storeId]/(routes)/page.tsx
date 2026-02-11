63dc607d83e197b9f2b5799fd6eda2f5771f3649
Arjuna Dwi Wijaksono <arjunadwi.w07@gmail.com>
2026-02-12 00:12:57 +0700

    chore: remove greeting text from dashboard

--- toko-anjani/admin/src/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ toko-anjani/admin/src/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -94,9 +94,6 @@ const DashboardPage = async ({
         <div className="flex items-center justify-between space-y-2">
           <div>
             <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
-            <p className="text-muted-foreground">
-              Selamat datang di {store?.name}
-            </p>
           </div>
         </div>
 
